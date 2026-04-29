379dc8489309c959ed37c5fab091c8ffb75f9a12
Muzzaiyyan Hussain <muzzaiyyan.web3@gmail.com>
2026-04-29 13:36:59 +0530

    added clerk auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-<h1 className="text-3xl">Hello</h1>
+      <UserButton/>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"], weight:['400', '500', '600', '700'], variable:"--font-ibm-plex" });
+const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"], weight: ['400', '500', '600', '700'], variable: "--font-ibm-plex" });
 
 export const metadata: Metadata = {
   title: "Imaginify",
@@ -16,8 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}</body>
-    </html>
+       <ClerkProvider afterSignOutUrl={'/sign-in'} appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+
   );
 }
